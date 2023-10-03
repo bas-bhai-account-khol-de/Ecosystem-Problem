ca1f0b6e8007e0daad3c691ae441ca06cf914b35
Prakhar <89300995+bas-bhai-account-khol-de@users.noreply.github.com>
2023-10-03 19:10:56 +0530

    example test

--- organismInput.py
+++ organismInput.py
@@ -3,33 +3,39 @@ import OrganismClass
 o = OrganismClass.organism
 
 #names 
-plantA = "plantA"
-plantB = "plantB"
-plantC = "plantC"
+Coral = "Coral"
+hornwort = "hornwort"
+Java = "Java"
 
-AnimalA= "AnimalA"
-AnimalB= "AnimalB"
-AnimalC= "AnimalC"
-AnimalD= "AnimalD"
-AnimalE= "AnimalE"
-AnimalF= "AnimalF"
-AnimalG= "AnimalG"
+BlueTang= "BlueTang"
+BullShark= "BullShark"
+GiantTigerPrawn= "GiantTigerPrawn"
+GultStur= "GultStur"
+Guppy= "Guppy"
+JapneseSpider= "JapneseSpider"
+JellyFish= "JellyFish"
+NorthenPike= "NorthenPike"
+Orca= "Orca"
+RainbowTrout= "RainbowTrout"
 
 
 plant_list=[
-    o(plantA,0,200),
-    o(plantB,0,200),
-    o(plantC,0,200),
+    o(Coral,0,6140),
+    o(hornwort,0,4930),
+    o(Java,0,5730),
 ]
 
 org_list=[
-    o(AnimalA,70,300,[plantB,AnimalC]),
-    o(AnimalB,70,100,[AnimalA,plantC]),
-    o(AnimalC,70,100,[AnimalB,AnimalD]),
-    o(AnimalD,70,100,[plantA,AnimalC]),
-    o(AnimalE,70,100,[AnimalB,AnimalC,AnimalA,AnimalD]),
-    o(AnimalF,70,100,[AnimalB,AnimalE,AnimalA]),
-    o(AnimalG,70,100,[AnimalB,AnimalA,AnimalD])
+    o(BlueTang,1770,3770,[JapneseSpider,GiantTigerPrawn,BullShark,NorthenPike,Guppy,GultStur,Orca]),
+    o(BullShark,1870,1100,[hornwort,Java]),
+    o(GiantTigerPrawn,1220,1520,[hornwort,Java,Coral]),
+    o(GultStur,1140,2960,[Orca,BullShark,hornwort,Coral,NorthenPike,Java]),
+    o(Guppy,1570,3230,[Orca,BullShark,hornwort,Coral,NorthenPike,Java]),
+    o(JapneseSpider,2200,4600,[hornwort]),
+    o(NorthenPike,1810,2700,[hornwort,Java]),
+    o(Orca,1700,3280,[hornwort,Java]),
+    o(RainbowTrout,4030,4650,[Guppy,JapneseSpider,GultStur,Orca,GiantTigerPrawn,NorthenPike]),
+    o(JellyFish,3430,3970,[hornwort])
 ]
 
 number_of_animals_to_be_selected =5
